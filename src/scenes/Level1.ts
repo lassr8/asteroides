1d5d3bb74767f8253d24f9db1c5a69987957b9be
pssr8 <sirSSR8@protonmail.com>
2023-09-02 13:58:30 -0400

    camera on center

--- src/scenes/Level1.ts
+++ src/scenes/Level1.ts
@@ -2,11 +2,17 @@ import GameScene from "./templates/GameScene";
 
 export default class Level1 extends GameScene {
     preload(): void {
-        
+        super.preload()        
+
     }
     create(): void {
-        
+        super.create()
+
     }
-    update(): void {
+    update(time: number, delta: number): void {
+        super.update(time, delta)
+
+        this.cameras.main.centerOn(0, 0);
+
     }
 }
--- src/scenes/templates/GameScene.ts
+++ src/scenes/templates/GameScene.ts
@@ -4,4 +4,25 @@ export default class GameScene extends Scene {
     constructor(config: {}) {
         super(config);
     }
+
+    bordes = {
+        width: 0,
+        height: 0
+    }
+
+    preload(): void {
+        this.bordes = {
+            width: this.game.canvas.width,
+            height: this.game.canvas.height
+        }
+        
+    }
+
+    create(): void {
+        
+    }
+
+    update(time: number, delta: number): void {
+        
+    }
 }
