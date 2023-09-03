bfe971c17947529dc6940a599c2ada0db958cc2d
lassr8 <lassr8@proton.me>
2023-09-03 15:01:36 -0400

    .

--- src/scenes/LostScene.ts
+++ src/scenes/LostScene.ts
@@ -8,12 +8,12 @@ export default class LostScene extends UIScene {
     space!: Phaser.Input.Keyboard.Key;
 
     create(): void {
-        let text = this.add.text(10, 10, 'PERDISTE', { fontSize: '30pt' })
+        this.add.text(10, 10, 'PERDISTE', { fontSize: '30pt' })
 
         this.space = this.input.keyboard!.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
 
-    update(time: number, delta: number): void {
+    update(): void {
         if (this.space.isDown) {
             this.scene.stop().start('LEVEL-1');
         }
